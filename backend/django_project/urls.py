807937bf2f044dfca6abd0d16628ba965b4994e8
benjamin schreiber <bpschreiber2003@gmail.com>
2023-09-08 15:51:54 -0700

    CRUD for HelloWorld and an Entry Serializer

--- backend/django_project/urls.py
+++ backend/django_project/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
         "swagger<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
-    path("hello-world/", HelloWorld.as_view()),
+    path("hello-world/", HelloWorld.as_view(), name="hello-world"),
+
 ]
