589ef8459be2b802077bc7495901545847559933
benjamin schreiber <bpschreiber2003@gmail.com>
2023-09-08 11:51:31 -0700

    Basic Key,Value entry for model

--- backend/pages/models.py
+++ backend/pages/models.py
@@ -1,13 +1,5 @@
 from djongo import models
 
-class Blog(models.Model):
-    name = models.CharField(max_length=100)
-
-    class Meta:
-        abstract = True
-
 class Entry(models.Model):
-    blog = models.EmbeddedField(
-        model_container=Blog
-    )    
-    headline = models.CharField(max_length=255)    
+    key = models.CharField(max_length=100)
+    value = models.CharField(max_length=100)
