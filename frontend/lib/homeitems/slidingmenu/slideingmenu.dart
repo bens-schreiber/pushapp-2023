de2c66b7cd034b980a1e013e322f92395ddd8c40
KyanPepper <kyankks1@gmail.com>
2023-11-11 20:37:04 -0800

    Sliding menu v1

--- frontend/lib/homeitems/slidingmenu/slideingmenu.dart
+++ frontend/lib/homeitems/slidingmenu/slideingmenu.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 
+class MenuBar extends StatelessWidget {
+  const MenuBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+  
+    );
+  }
+}
+
 class Menu extends StatelessWidget {
   const Menu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      height: 200,
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text("icon placements"),
+            ElevatedButton(
+              child: const Text('^'),
+              onPressed: () => Navigator.pop(context),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
