28add45394443bbef46cc4aea9bcc362a464af33
KyanPepper <kyankks1@gmail.com>
2023-11-11 21:35:09 -0800

    Bottom bar with button (not scrollable)

--- frontend/lib/homeitems/home.dart
+++ frontend/lib/homeitems/home.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:frontend/homeitems/slidingmenu/slideingmenu.dart';
 
+
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Menu();
+    return Scaffold(
+      bottomSheet: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return Menu();
+              },
+            );
+          }, child: const Text("^^^"),
+        ),
+      ),
+    );
   }
 }
--- frontend/lib/homeitems/slidingmenu/slideingmenu.dart
+++ frontend/lib/homeitems/slidingmenu/slideingmenu.dart
@@ -5,27 +5,32 @@ class MenuBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-  
+    return DraggableScrollableSheet(
+      builder: (context, ScrollController scrollController) =>
+          SingleChildScrollView(
+        controller: scrollController,
+        child: Menu(),
+      ),
     );
   }
 }
 
 class Menu extends StatelessWidget {
-  const Menu({super.key});
+  const Menu({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       height: 200,
+      color: Colors.amber,
       child: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.min,
           children: <Widget>[
-            const Text("icon placements"),
+            const Text('Bottom Sheet'),
             ElevatedButton(
-              child: const Text('^'),
+              child: const Text('Close'),
               onPressed: () => Navigator.pop(context),
             ),
           ],
