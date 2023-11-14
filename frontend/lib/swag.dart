f31d29d67242a93673a554a3728ceec4647fc4b6
Ben Schreiber <bpschreiber2003@gmail.com>
2023-11-13 17:45:51 -0800

    swagger generator

--- frontend/lib/swag.dart
+++ frontend/lib/swag.dart
@@ -14,6 +14,6 @@ import 'package:openapi_generator_annotations/openapi_generator_annotations.dart
   update to the latest swagger doc. The following line is a random hash to act
   as the file edit, and should be automated via the generate-swagger.ps1 file. 
 */
-// HASH: W0JNeVaGDtmc1844fHT2RFfEg2vifDG5
+// HASH: Ulu3mQUUIA5vmhD3CEM9rrhnn3cejtb2
 // ignore: deprecated_member_use
 class SwaggerGenerator extends OpenapiGeneratorConfig {}
