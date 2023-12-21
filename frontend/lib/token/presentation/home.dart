51294bee0ce4b57172daae94dd8e9341a6b1b9f2
KyanPepper <kyankks1@gmail.com>
2023-12-20 16:24:02 -0800

    Circles chnage depeanding on screen

--- frontend/lib/token/presentation/home.dart
+++ frontend/lib/token/presentation/home.dart
@@ -3,13 +3,34 @@ import 'package:frontend/token/presentation/bottom_sheet/bottom_sheet_button.dar
 import 'package:frontend/token/presentation/circles.dart';
 
 class Home extends StatelessWidget {
-  const Home({super.key});
+  const Home({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        body: Stack(
-            children: [Positioned(left: -136, top: 25, child: Circles())]),
-        bottomSheet: BottomSheetButton());
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+    final iphonewidth = 430.0;
+    final iphoneheight = 932.0;
+    final widthRatio = screenWidth / iphonewidth;
+    final heightRatio = screenHeight / iphoneheight;
+    final adjustedLeft = -145 * widthRatio;
+    final adjustedTop = 25 * heightRatio;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Positioned(
+            left: adjustedLeft,
+            top: adjustedTop,
+            child: Container(
+              width: 702,
+              height: 702,
+              child: Circles(),
+            ),
+          ),
+        ],
+      ),
+      bottomSheet: const BottomSheetButton(),
+    );
   }
 }
